4d6923b973fb91f69881cb6ab6899fc24f5e4678
Timmo <contact@timmo.xyz>
2019-04-09 16:24:26 +0100

    :arrow_up: Bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ovoenergy",
-    version="0.1.0",
+    version="0.1.2",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="",
