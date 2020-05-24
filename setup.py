21ea3c4a0184c37a1de0e6edadbb9a0ed7361bcf
Timmo <contact@timmo.xyz>
2020-05-24 23:31:33 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ovoenergy",
-    version="1.1.4",
+    version="1.1.5",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="Get energy data from OVO's API",
