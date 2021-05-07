dc2b13aaf05a7b693b52067865bebe08f759abd1
Aidan Timson <contact@timmo.xyz>
2021-05-07 22:44:49 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ovoenergy",
-    version="1.1.11",
+    version="1.1.12",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="Get energy data from OVO's API",
