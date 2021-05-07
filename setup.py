326b52c037b35700a80b4efbca5155877513fc0f
Aidan Timson <contact@timmo.xyz>
2021-05-07 22:40:13 +0100

    Format with black

--- setup.py
+++ setup.py
@@ -12,20 +12,13 @@ setuptools.setup(
     description="Get energy data from OVO's API",
     long_description=LONG,
     long_description_content_type="text/markdown",
-    install_requires=[
-        'aiohttp>=3.7.3',
-        'click>=7.1.2'
-    ],
-    entry_points={
-        'console_scripts': [
-            'ovoenergy = ovoenergy.cli:cli'
-        ]
-    },
+    install_requires=["aiohttp>=3.7.3", "click>=7.1.2"],
+    entry_points={"console_scripts": ["ovoenergy = ovoenergy.cli:cli"]},
     url="https://github.com/timmo001/ovoenergy",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    )
+    ),
 )
