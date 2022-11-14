f53cb8f8986becb13510bdf948e5fa84100f0ecf
Aidan Timson <contact@timmo.xyz>
2022-11-14 12:04:04 +0000

    Convert classes to pydantic classes

--- ovoenergy/__init__.py
+++ ovoenergy/__init__.py
@@ -1,74 +1,80 @@
 """Initialize the package."""
+from __future__ import annotations
+
 from datetime import datetime
 from typing import List
 
-
-class OVOInterval:
-    def __init__(self, start: datetime, end: datetime):
-        self.start = start
-        self.end = end
+from pydantic import BaseModel, Extra, Field
 
 
-class OVOMeterReadings:
-    def __init__(self, start: float, end: float):
-        self.start = start
-        self.end = end
+class OVOBase(BaseModel):
+    """Base class for all OVO models."""
+
+    class Config:
+        """Pydantic config."""
+
+        extra = Extra.allow
 
 
-class OVOCost:
-    def __init__(self, amount: float, currency_unit: str):
-        self.amount = amount
-        self.currency_unit = currency_unit
+class OVOInterval(OVOBase):
+    """Interval model."""
+
+    start: datetime = Field(..., alias="start")
+    end: datetime = Field(..., alias="end")
 
 
-class OVODailyElectricity:
-    def __init__(
-        self,
-        consumption: float,
-        interval: OVOInterval,
-        meter_readings: OVOMeterReadings,
-        has_half_hour_data: bool,
-        cost: OVOCost,
-    ):
-        self.consumption = consumption
-        self.interval = interval
-        self.meter_readings = meter_readings
-        self.has_half_hour_data = has_half_hour_data
-        self.cost = cost
+class OVOMeterReadings(OVOBase):
+    """Meter readings model."""
+
+    start: float = Field(..., alias="start")
+    end: float = Field(..., alias="end")
 
 
-class OVODailyGas:
-    def __init__(
-        self,
-        consumption: float,
-        volume: float,
-        interval: OVOInterval,
-        meter_readings: OVOMeterReadings,
-        has_half_hour_data: bool,
-        cost: OVOCost,
-    ):
-        self.consumption = consumption
-        self.volume = volume
-        self.interval = interval
-        self.meter_readings = meter_readings
-        self.has_half_hour_data = has_half_hour_data
-        self.cost = cost
+class OVOCost(OVOBase):
+    """Cost model."""
+
+    amount: float = Field(..., alias="amount")
+    currency_unit: str = Field(..., alias="currencyUnit")
 
 
-class OVOHalfHour:
-    def __init__(self, consumption: float, interval: OVOInterval, unit: str):
-        self.consumption = consumption
-        self.interval = interval
-        self.unit = unit
+class OVODailyElectricity(OVOBase):
+    """Daily electricity model."""
+
+    consumption: float = Field(..., alias="consumption")
+    interval: OVOInterval = Field(..., alias="interval")
+    meter_readings: OVOMeterReadings = Field(..., alias="meterReadings")
+    has_half_hour_data: bool = Field(..., alias="hasHalfHourData")
+    cost: OVOCost = Field(..., alias="cost")
 
 
-class OVODailyUsage:
-    def __init__(self, electricity: List[OVODailyElectricity], gas: List[OVODailyGas]):
-        self.electricity = electricity
-        self.gas = gas
+class OVODailyGas(OVOBase):
+    """Daily gas model."""
+
+    consumption: float = Field(..., alias="consumption")
+    volume: float = Field(..., alias="volume")
+    interval: OVOInterval = Field(..., alias="interval")
+    meter_readings: OVOMeterReadings = Field(..., alias="meterReadings")
+    has_half_hour_data: bool = Field(..., alias="hasHalfHourData")
+    cost: OVOCost = Field(..., alias="cost")
 
 
-class OVOHalfHourUsage:
-    def __init__(self, electricity: List[OVOHalfHour], gas: List[OVOHalfHour]):
-        self.electricity = electricity
-        self.gas = gas
+class OVOHalfHour(OVOBase):
+    """Half hour model."""
+
+    consumption: float = Field(..., alias="consumption")
+    interval: OVOInterval = Field(..., alias="interval")
+    unit: str = Field(..., alias="unit")
+
+
+class OVODailyUsage(OVOBase):
+    """Daily usage model."""
+
+    electricity: List[OVODailyElectricity] = Field(..., alias="electricity")
+    gas: List[OVODailyGas] = Field(..., alias="gas")
+
+
+class OVOHalfHourUsage(OVOBase):
+    """Half hour usage model."""
+
+    electricity: List[OVOHalfHour] = Field(..., alias="electricity")
+    gas: List[OVOHalfHour] = Field(..., alias="gas")
