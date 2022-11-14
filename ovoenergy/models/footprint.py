e35539b4feeb60474ef01a1f21eb6ce8139992cd
Aidan Timson <contact@timmo.xyz>
2022-11-14 14:50:11 +0000

    Add missing doctrings

--- ovoenergy/models/footprint.py
+++ ovoenergy/models/footprint.py
@@ -9,23 +9,31 @@ from . import OVOBase
 
 
 class OVOFootprintElectricity(OVOBase):
+    """Electricity footprint model."""
+
     carbon_kg: float = Field(None, alias="carbonKg")
     carbon_saved_kg: float = Field(None, alias="carbonSavedKg")
     k_wh: float = Field(None, alias="kWh")
 
 
 class OVOFootprintGas(OVOBase):
+    """Gas footprint model."""
+
     carbon_kg: float = Field(None, alias="carbonKg")
     carbon_saved_kg: float = Field(None, alias="carbonSavedKg")
     k_wh: float = Field(None, alias="kWh")
 
 
 class OVOFootprintBreakdown(OVOBase):
+    """Footprint breakdown model."""
+
     electricity: OVOFootprintElectricity = Field(None, alias="electricity")
     gas: OVOFootprintGas = Field(None, alias="gas")
 
 
 class OVOCarbonFootprint(OVOBase):
+    """Carbon footprint model."""
+
     carbon_kg: float = Field(None, alias="carbonKg")
     carbon_saved_kg: float = Field(None, alias="carbonSavedKg")
     k_wh: float = Field(None, alias="kWh")
@@ -33,6 +41,8 @@ class OVOCarbonFootprint(OVOBase):
 
 
 class OVOFootprint(OVOBase):
+    """Footprint model."""
+
     from_: Optional[str] = Field(None, alias="from")
     to: Optional[str] = Field(None, alias="to")
     carbon_reduction_product_ids: Optional[List] = Field(
