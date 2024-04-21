c7c5b964a5edef70f3cf23516e26ca5fbf8d4930
Aidan Timson <aidan@timmo.dev>
2024-04-21 09:02:37 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-"""Setup"""
-import io
+"""Setup."""
 
 from setuptools import find_packages, setup
 
 # Get setup packages from requirements.txt
-with io.open("requirements_setup.txt", encoding="utf-8") as f:
+with open("requirements_setup.txt", encoding="utf-8") as f:
     requirements_setup = f.read().splitlines()
 
 # Get packages from requirements.txt
-with io.open("requirements.txt", encoding="utf-8") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
 
 setup(
     name="ovoenergy",
-    description="OVO Energy",
-    keywords="python ovoenergy",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
-    license="MIT",
+    description="OVO Energy",
+    keywords="python,ovoenergy,api",
+    license="Apache-2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/timmo001/ovoenergy",
-    packages=find_packages(exclude=["tests", "generator"]),
     install_requires=requirements,
+    packages=find_packages(exclude=["tests", "generator"]),
+    python_requires=">=3.11",
     setup_requires=requirements_setup,
     use_incremental=True,
 )
