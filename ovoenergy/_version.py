98d8c9c95a0c86699fc2e70513dc16c9a544e5b5
Aidan Timson <contact@timmo.xyz>
2024-04-21 12:10:45 +0100

    2.0.0.dev0

--- ovoenergy/_version.py
+++ ovoenergy/_version.py
@@ -7,5 +7,5 @@ Provides ovoenergy version information.
 
 from incremental import Version
 
-__version__ = Version("ovoenergy", 1, 3, 1)
+__version__ = Version("ovoenergy", 2, 0, 0, dev=0)
 __all__ = ["__version__"]
