c993b8e0a3867eea7ea930073bfee0da520cfefe
timmo001 <timmo001@users.noreply.github.com>
2025-06-18 21:27:23 +0000

    Bump ovoenergy version to 2.0.2.dev0

--- ovoenergy/_version.py
+++ ovoenergy/_version.py
@@ -7,5 +7,5 @@ Provides ovoenergy version information.
 
 from incremental import Version
 
-__version__ = Version("ovoenergy", 2, 0, 1)
+__version__ = Version("ovoenergy", 2, 0, 2, dev=0)
 __all__ = ["__version__"]
