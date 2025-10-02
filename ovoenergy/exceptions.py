c19f589a06b509a5076aa7cc46f8f5ecce6b2b9b
Aidan Timson <aidan@timmo.dev>
2025-10-02 17:04:22 +0100

    Fix

--- ovoenergy/exceptions.py
+++ ovoenergy/exceptions.py
@@ -14,6 +14,7 @@ class OVOEnergyNoAccount(OVOEnergyException):
 class OVOEnergyAPIException(OVOEnergyException):
     """Exception for API exceptions."""
 
+
 class OVOEnergyAPIInvalidResponse(OVOEnergyAPIException):
     """Exception for invalid response."""
 
